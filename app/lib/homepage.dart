15f02ea1975369ff82cda3863498848af28e2350
cecahn <a.cecilia.ahnlund@gmail.com>
2023-03-22 23:42:49 +0100

    first-app-version

---
+++ app/lib/homepage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+    const HomePage({Key? key}) : super(key: key);
+
+    @override
+    _HomePageState createState() => _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage> {
+
+final _textController = TextEditingController();
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.all(100.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    hintText: 'Skriv QR kod för din mat',
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                            _textController.clear();
+                        },
+                        icon: const Icon(Icons.clear),
+                    ),
+                    ),
+                )
+            ],
+        ),
+        )
+    );
+  }
+
+}
