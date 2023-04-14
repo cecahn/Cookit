ec79b8b8f5714f93bfb8eeddbb35238af3efebad
Einar Johansson <ejrobb09@gmail.com>
2023-04-14 21:43:46 +0200

    tog bort kod som ska vara i front-end.

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 # Third-party libraries
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 from flask_login import (
@@ -9,8 +9,6 @@ from flask_login import (
     login_user,
     logout_user,
 )
-import json
-from oauthlib.oauth2 import WebApplicationClient
 import requests
 
 # Internal imports
@@ -47,26 +45,6 @@ CORS(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
-
-# Flask-Login helper to retrieve a user from our db
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id, mysql)
-
-@app.route("/")
-def index():
-    if current_user.is_authenticated:
-        return (
-            "<p>Hello! You're logged in! Email: {}</p>"
-            '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.email
-            )
-        )
-    else:
-        return '<a class="button" href="/login">Google Login</a>'
-
-
 @app.route("/skafferi/spara")
 @login_required
 def fetch_product():
@@ -99,6 +77,7 @@ def fetch_product():
 
     return product
 
+
 @app.route("/skafferi/ta_bort")
 @login_required
 def delete_from_pantry():
@@ -109,7 +88,6 @@ def delete_from_pantry():
         return "OK"
     
     return "Kunde inte ta bort varan"
-
 
 
 def valid_gtin(gtin: str):
@@ -133,78 +111,46 @@ def get_skafferi():
 
 # AUTH ROUTES ================================================================
 
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id, mysql)
+
+
 @app.route("/login")
 def login():
-    # Find out what URL to hit for Google login
-    google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    # hämta token
+    token = request.args.get('access_token')
+    if (not token):
+        return 'Must provide access token', 400
 
-    # Use library to construct the request for Google login and provide
-    # scopes that let you retrieve user's profile from Google
-    request_uri = client.prepare_request_uri(
-        authorization_endpoint,
-        redirect_uri=request.base_url + "/callback",
-        scope=["openid", "email", "profile"],
-    )
+    # hämta uppgifter från google
+    r=requests.get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", headers={"Authorization":f'Bearer {token}'})
 
-    return redirect(request_uri)
+    if (r.status_code != 200):
+        return 'Access token not valid!', 401
 
-def get_google_provider_cfg():
-    return requests.get(GOOGLE_DISCOVERY_URL).json()
+    profile = r.json()
+
+    unique_id = profile['id']
+    users_email = profile['email']
+
+    user = User(unique_id, users_email)
+
+    if not User.get(unique_id, mysql):
+        User.create(unique_id, users_email, mysql)
+
+    login_user(user)
+
+    return "Success, du är inloggad!", 200
+
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("index"))
-
-@app.route("/login/callback")
-def callback():
-    # Get authorization code Google sent back to you
-    code = request.args.get("code")
-    # Find out what URL to hit to get tokens that allow you to ask for
-    # things on behalf of a user
-    google_provider_cfg = get_google_provider_cfg()
-    token_endpoint = google_provider_cfg["token_endpoint"]
-
-    # Prepare and send a request to get tokens! Yay tokens!
-    token_url, headers, body = client.prepare_token_request(
-        token_endpoint,
-        authorization_response=request.url,
-        redirect_url=request.base_url,
-        code=code
-    )
-    token_response = requests.post(
-        token_url,
-        headers=headers,
-        data=body,
-        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
-    )
-
-    # Parse the tokens!
-    client.parse_request_body_response(json.dumps(token_response.json()))
-    
-    # Now that you have tokens (yay) let's find and hit the URL
-    # from Google that gives you the user's profile information,
-    # including their Google profile image and email
-    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-    uri, headers, body = client.add_token(userinfo_endpoint)
-    userinfo_response = requests.get(uri, headers=headers, data=body)
-
-    unique_id = userinfo_response.json()["sub"]
-    users_email = userinfo_response.json()["email"]
-    print(unique_id)
-    print(users_email)
-
-    user = User(unique_id, users_email)
-    
-    if not User.get(unique_id, mysql):
-        User.create(unique_id, users_email, mysql)
-    
-    login_user(user)
-
-    return redirect(url_for("index"))
+    return 'Logged out', 200
 
 
 if __name__ == '__main__':
-    app.run(ssl_context="adhoc", threaded=True)
+    app.run(threaded=True)
